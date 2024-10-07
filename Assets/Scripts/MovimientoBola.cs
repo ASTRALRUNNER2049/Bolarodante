faa14ee63e44357cb9a1de40151a810e2eb78629
ASTRALRUNNER2049 <astraldisaster1635@gmail.com>
2024-10-07 18:24:18 +0200

    Bolarodante_7

--- Assets/Scripts/MovimientoBola.cs
+++ Assets/Scripts/MovimientoBola.cs
@@ -7,11 +7,27 @@ public class MovimientoBola : MonoBehaviour
     public GameObject Player;
     public Rigidbody bolaRigidbody;
     public Respawn Respawn;
-    public int vidas = 3;
+    public int vidas = 100;
     public int monedas = 0;
+    public Rigidbody rb;
+    public float fuerzasalto = 8f;
+    public bool Salto;
 
     public float verticalVelocity;
     public float horizontalVelocity;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Salto = false;
+        vidas = 3;
+        monedas = 0;    
+    }
+    private void FixedUpdate()
+    {
+        
+
+    }
 
     void Update()
     {
@@ -40,13 +56,14 @@ public class MovimientoBola : MonoBehaviour
     {
         if (collision.gameObject.tag == "Muerte")
         {
-            vidas = -1;
+            
+            vidas--;
             Respawn.MuerteBola();
             Debug.Log("Has Muerto, tienes -1 Vida");
            
             if (vidas == 0)
             {
-               
+              Destroy(gameObject);
             }
 
 
@@ -56,7 +73,7 @@ public class MovimientoBola : MonoBehaviour
 
         if (collision.gameObject.tag == "Moneda")
         {
-            monedas = +1;
+            monedas++;
             Debug.Log("Has cogido 1 moneda");
         }
 
