bcac126326f6e0415289a6a028c1131c5d5d6d00
ASTRALRUNNER2049 <astraldisaster1635@gmail.com>
2024-11-07 13:12:38 +0100

    Bolasrodantes_13

---
+++ Assets/Scripts/Movimiento cabeza.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimientocabeza : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f * Time.deltaTime, 0f * Time.deltaTime, 25f * Time.deltaTime);
+    }
+}
